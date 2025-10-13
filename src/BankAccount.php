cfe4403541598919f40cb4ab522913b1894c722f
patricia <patricia.gsbcn@gmail.com>
2025-10-13 18:42:21 +0200

    2

---
+++ src/BankAccount.php
@@ -0,0 +1,43 @@
+<?php
+
+class BankAccount
+{
+
+    public float $balance = 0;
+    public bool $status;
+
+    public function __construct(float $initialBalance = 200.0) {
+        $this->balance = $initialBalance;
+        $this->isOpen = true;
+        echo "My balance: ". $this->balance."\n";
+    }
+
+    public function getBalance(): float{
+        return $this-> balance;
+    }        
+            
+    
+    public function reopenAccount(): void{
+         if (!$this->isOpen) {
+            $this->isOpen = true;
+            echo " Reopen " . ($this->isOpen ? 'Open' : 'Closed') . "\n";
+        } else {
+            echo " Error\n";
+        }
+
+    }
+    
+    public function closeAccount():void{
+        if ($this->isOpen) {
+        $this->isOpen = false;
+        echo " Close " . ($this->isOpen ? 'Open' : 'Closed') . "\n";
+        } else {
+            echo "Error.\n";
+        }
+    }
+    
+    public function getAccount():float{
+        $balance = 0;
+    }
+        
+}
